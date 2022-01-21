16703675308414e3b439e304f3da1135abadbac1
techsolutionsx2 <benjaminmoritz8888@gmail.com>
2022-01-21 11:42:29 +0900

    herokucomplet

--- src/views/Home/Work/Work.styled.tsx
+++ src/views/Home/Work/Work.styled.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 // -------------------------------------------------------
 export const Layout = styled.div`
   margin-top: 150px;
-  width: 70%;
+  width: 90%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 50px;
